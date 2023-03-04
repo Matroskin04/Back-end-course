b049cbe1a755ec27b2c2815dfd70220845011a48
Matroskin04 <zetwyn@mail.ru>
2023-03-04 20:45:47 +0700

    04.03.23

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express'
 import { productsRouter } from './routers/products-router';
+import { runDb } from './repositories/db';
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -9,11 +10,16 @@ app.use(parserMiddleware)
 
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello Samurai!!!')
+    res.send('Hello Samurai')
 })
 
 app.use('/products', productsRouter)
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+const startApp = async () => {
+    await runDb()
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`)
+    })
+}
+
+startApp()
