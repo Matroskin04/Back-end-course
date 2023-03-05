ed0d62ad96775e039e2c8323000ff5be523200d3
Matroskin04 <zetwyn@mail.ru>
2023-03-06 00:42:07 +0700

    testing get and post

--- src/index.ts
+++ src/index.ts
@@ -2,59 +2,112 @@ import express, {Request, Response} from 'express'
 import bodyParser from "body-parser";
 
 const app = express()
-const port = 3000
+const port = 3002
 
-const students = [{id: 1, name: 'Vlad'}, {id: 2, name: 'Nikita'}]
+type videoType = {
+    "id": number,
+    "title": string,
+    "author": string,
+    "canBeDownloaded": boolean,
+    "minAgeRestriction": any,
+    "createdAt": string,
+    "publicationDate": string,
+    "availableResolutions": Array<string>
+};
+type allVideosType = Array<videoType>;
+type errorObjType = {"message": string, "field": string};
+type errorArrType = Array<errorObjType>;
+type errorType = {"errorsMessages": errorArrType};
 
+let allVideos: allVideosType= [];
+const createdAt = new Date().toISOString();
+const publicationDate = new Date(Date.now() + ( 3600 * 1000 * 24)).toISOString();
+const availableResolutions = [ 'P144', 'P240', 'P360', 'P480',
+    'P720', 'P1080', 'P1440', 'P2160' ]
 
 const parserMiddeleware = bodyParser({})
 app.use(parserMiddeleware)
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!')
+app.get('/hometask-01/videos', (req: Request, res: Response) => {
+    res.status(200).send(allVideos)
 })
-app.get('/students', (req: Request, res: Response) => {
-    if (req.query.name) {
-        let partName = req.query.name.toString();
-        res.send(students.filter(p => p.name.indexOf(partName) !== -1))
+app.post('/hometask-01/videos', (req: Request, res: Response) => {
+    if (req.body.title.length > 40) {
+        let currentError: errorType = {
+            "errorsMessages": [
+                {
+                    "message": 'The string must be less than 40 characters',
+                    "field": 'title'
+                }
+            ]
+        };
+        res.status(400).send(currentError)
+    } else if (typeof req.body.title !== 'string') {
+        let currentError: errorType = {
+            "errorsMessages": [
+                {
+                    "message": 'The type must be string',
+                    "field": 'title'
+                }
+            ]
+        };
+        res.status(400).send(currentError)
+    } else if (req.body.author.length > 20) {
+        let currentError: errorType = {
+            "errorsMessages": [
+                {
+                    "message": 'The string must be less than 20 characters',
+                    "field": 'author'
+                }
+            ]
+        };
+        res.status(400).send(currentError)
+    } else if (typeof req.body.author !== 'string') {
+        let currentError: errorType = {
+            "errorsMessages": [
+                {
+                    "message": 'The type must be string',
+                    "field": 'author'
+                }
+            ]
+        };
+        res.status(400).send(currentError)
+    }else if (req.body.availableResolutions.length === 0) {
+        let currentError: errorType = {
+            "errorsMessages": [
+                {
+                    "message": 'availableResolutions must contain any information',
+                    "field": 'availableResolutions'
+                }
+            ]
+        };
+        res.status(400).send(currentError)
+    } else if (!req.body.availableResolutions.every((p: string) => availableResolutions.includes(p)) ) {
+        let currentError: errorType = {
+            "errorsMessages": [
+                {
+                    "message": 'availableResolutions must contain variants from suggested',
+                    "field": 'availableResolutions'
+                }
+            ]
+        };
+        res.status(400).send(currentError)
     } else {
-        res.send(students)
+        const newVideo: videoType = {
+            "id": allVideos[allVideos.length - 1]?.id + 1 || 0,
+            "title": req.body.title,
+            "author": req.body.author,
+            "canBeDownloaded": true,
+            "minAgeRestriction": 5,
+            "createdAt": createdAt,
+            "publicationDate": publicationDate,
+            "availableResolutions": req.body.availableResolutions
+        };
+        allVideos.push(newVideo);
+        res.status(201).send(newVideo);
     }
 })
-app.get('/students/:currentStudent', (req: Request, res: Response) => {
-    let student = students.find(p => p.name === req.params.currentStudent)
-    if (!student) res.send(404)
-    else res.send(student)
-})
-app.delete('/students/:id', (req: Request, res: Response) => {
-    for (let i = 0; i < students.length; i++) {
-        if (students[i].id === +req.params.id) {
-            students.splice(i,1);
-            res.send(204);
-            return;
-        }
-    }
-    res.send(404)
-})
-app.post('/students', (req: Request, res: Response) => {
-    const newStudent = {
-        id: +(new Date()),
-        name: req.body.name
-    }
-    students.push(newStudent)
-    res.status(201).send(newStudent)
-})
-app.put('/students/:elem', (req: Request, res: Response) => {
-    let elem = +req.params.elem
-    if (elem < students.length) {
-        students[elem].name = req.body.name
-        res.status(200).send(students[elem])
-    } else {
-        res.send(404)
-    }
-})
-
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-}) 
+})
