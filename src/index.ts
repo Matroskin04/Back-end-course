b2448c06579f16d6f7122fe6b6a8d70d2036c807
Matroskin04 <zetwyn@mail.ru>
2023-03-07 00:33:52 +0700

    testing get and post

--- src/index.ts
+++ src/index.ts
@@ -14,24 +14,37 @@ type videoType = {
     "publicationDate": string,
     "availableResolutions": Array<string>
 };
+/*type bodyType = {
+    "id"?: number,
+    "title"?: string,
+    "author"?: string,
+    "canBeDownloaded"?: boolean,
+    "minAgeRestriction"?: any,
+    "createdAt"?: string,
+    "publicationDate"?: string,
+    "availableResolutions"?: Array<string>
+};*/
 type allVideosType = Array<videoType>;
 type errorObjType = { message: string, field: string };
 type errorType = { errorsMessages: Array<errorObjType> };
 
+
 let allVideos: allVideosType = [];
 let arrErrors: Array<errorObjType> = [];
 let allErrors: errorType = {errorsMessages: arrErrors};
-const createdAt = new Date().toISOString();
-const publicationDate = new Date(Date.now() + (3600 * 1000 * 24)).toISOString();
+const now = new Date()
+const createdAt = now.toISOString();
+const publicationDate = new Date(now.setDate(now.getDate() + 1)).toISOString();
 const availableResolutions = ['P144', 'P240', 'P360', 'P480',
     'P720', 'P1080', 'P1440', 'P2160']
+
 
 const parserMiddeleware = bodyParser({})
 app.use(parserMiddeleware)
 
-const checkError = (body: any) => {
+
+const checkError = (body: any) => {  // тип длля body
     if (typeof body.title !== 'string') {
-        console.log('title')
         arrErrors.push({
                 message: 'The type must be string',
                 field: 'title'
@@ -93,6 +106,7 @@ const checkError = (body: any) => {
         )
     }
 }
+
 app.get('/', (req: Request, res: Response) => {
     res.send("Hello!!")
 })
@@ -107,7 +121,7 @@ app.post('/hometask-01/videos', (req: Request, res: Response) => {
         return;
     } else {
         const newVideo: videoType = {
-            "id": allVideos.at(-1) ? allVideos[allVideos.length - 1].id + 1 : 0,
+            "id": allVideos.at(-1) ? allVideos.at(-1)!.id + 1 : 0,
             "title": req.body.title,
             "author": req.body.author,
             "canBeDownloaded": req.body.canBeDownloaded ?? false,
@@ -134,7 +148,7 @@ app.put('/hometask-01/videos/:id', (req: Request, res: Response) => {
         if (key.id === +req.params.id) {
             checkError(req.body)
             if (arrErrors.length > 0) {
-                res.status(400).send(allErrors);
+                res.status(400).send(allErrors);            // в начало
                 arrErrors.length = 0;
                 return
             } else {
