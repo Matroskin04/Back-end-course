13521cfe3a7a27c3c189467fd6c70d58bc66f14c
Matroskin04 <zetwyn@mail.ru>
2023-03-06 14:55:51 +0700

    testing get and post

--- src/index.ts
+++ src/index.ts
@@ -15,15 +15,15 @@ type videoType = {
     "availableResolutions": Array<string>
 };
 type allVideosType = Array<videoType>;
-type errorObjType = {"message": string, "field": string};
+type errorObjType = { "message": string, "field": string };
 type errorArrType = Array<errorObjType>;
-type errorType = {"errorsMessages": errorArrType};
+type errorType = { "errorsMessages": errorArrType };
 
-let allVideos: allVideosType= [];
+let allVideos: allVideosType = [];
 const createdAt = new Date().toISOString();
-const publicationDate = new Date(Date.now() + ( 3600 * 1000 * 24)).toISOString();
-const availableResolutions = [ 'P144', 'P240', 'P360', 'P480',
-    'P720', 'P1080', 'P1440', 'P2160' ]
+const publicationDate = new Date(Date.now() + (3600 * 1000 * 24)).toISOString();
+const availableResolutions = ['P144', 'P240', 'P360', 'P480',
+    'P720', 'P1080', 'P1440', 'P2160']
 
 const parserMiddeleware = bodyParser({})
 app.use(parserMiddeleware)
@@ -75,7 +75,7 @@ app.post('/hometask-01/videos', (req: Request, res: Response) => {
             ]
         };
         res.status(400).send(currentError)
-    }else if (req.body.availableResolutions.length === 0) {
+    } else if (req.body.availableResolutions.length === 0) {
         let currentError: errorType = {
             "errorsMessages": [
                 {
@@ -85,7 +85,7 @@ app.post('/hometask-01/videos', (req: Request, res: Response) => {
             ]
         };
         res.status(400).send(currentError)
-    } else if (!req.body.availableResolutions.every((p: string) => availableResolutions.includes(p)) ) {
+    } else if (!req.body.availableResolutions.every((p: string) => availableResolutions.includes(p))) {
         let currentError: errorType = {
             "errorsMessages": [
                 {
@@ -128,6 +128,116 @@ app.get('/hometask-01/videos/:id', (req: Request, res: Response) => {
         }
     }
     res.send(400)
+})
+app.put('/hometask-01/videos/:id', (req: Request, res: Response) => {
+    for (let key of allVideos) {
+        if (key.id === +req.params.id) {
+            if (req.body.title.length > 40) {
+                let currentError: errorType = {
+                    "errorsMessages": [
+                        {
+                            "message": 'The string must be less than 40 characters',
+                            "field": 'title'
+                        }
+                    ]
+                };
+                res.status(400).send(currentError)
+            } else if (typeof req.body.title !== 'string') {
+                let currentError: errorType = {
+                    "errorsMessages": [
+                        {
+                            "message": 'The type must be string',
+                            "field": 'title'
+                        }
+                    ]
+                };
+                res.status(400).send(currentError)
+            } else if (req.body.author.length > 20) {
+                let currentError: errorType = {
+                    "errorsMessages": [
+                        {
+                            "message": 'The string must be less than 20 characters',
+                            "field": 'author'
+                        }
+                    ]
+                };
+                res.status(400).send(currentError)
+            } else if (typeof req.body.author !== 'string') {
+                let currentError: errorType = {
+                    "errorsMessages": [
+                        {
+                            "message": 'The type must be string',
+                            "field": 'author'
+                        }
+                    ]
+                };
+                res.status(400).send(currentError)
+            } else if (req.body.availableResolutions.length === 0) {
+                let currentError: errorType = {
+                    "errorsMessages": [
+                        {
+                            "message": 'availableResolutions must contain any information',
+                            "field": 'availableResolutions'
+                        }
+                    ]
+                };
+                res.status(400).send(currentError)
+            } else if (!req.body.availableResolutions.every((p: string) => availableResolutions.includes(p))) {
+                let currentError: errorType = {
+                    "errorsMessages": [
+                        {
+                            "message": 'availableResolutions must contain variants from suggested',
+                            "field": 'availableResolutions'
+                        }
+                    ]
+                };
+                res.status(400).send(currentError)
+            } else if (typeof req.body.minAgeRestriction !== 'number'
+                        || req.body.minAgeRestriction > 18
+                        || req.body.minAgeRestriction < 1) {
+                let currentError: errorType = {
+                    "errorsMessages": [
+                        {
+                            "message": 'Number must be from 1 to 18 characters',
+                            "field": 'minAgeRestriction'
+                        }
+                    ]
+                };
+                res.status(400).send(currentError)
+            } else if (req.body.canBeDownloaded !== 'true'
+                    && req.body.canBeDownloaded !== 'false') {
+                let currentError: errorType = {
+                    "errorsMessages": [
+                        {
+                            "message": 'canBeDownloaded must be boolean',
+                            "field": 'canBeDownloaded'
+                        }
+                    ]
+                };
+                res.status(400).send(currentError)
+            } else if (typeof req.body.publicationDate !== 'string') {
+                let currentError: errorType = {
+                    "errorsMessages": [
+                        {
+                            "message": 'canBeDownloaded must be boolean',
+                            "field": 'canBeDownloaded'
+                        }
+                    ]
+                };
+                res.status(400).send(currentError)
+            } else {
+            key.title = req.body.title ?? key.title;
+            key.author = req.body.author ?? key.author;
+            key.availableResolutions = req.body.availableResolutions ?? key.availableResolutions;
+            key.canBeDownloaded = req.body.canBeDownloaded ?? key.canBeDownloaded;
+            key.minAgeRestriction = req.body.minAgeRestriction ?? key.minAgeRestriction;
+            key.publicationDate = req.body.publicationDate ?? key.publicationDate;
+            res.send(204);
+            return;
+        }
+    }
+    res.send(404)
+}
 })
 
 
