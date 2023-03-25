b65f764681b98371d9dce27f16c92feb177ab96c
Matroskin04 <122979449+Matroskin04@users.noreply.github.com>
2023-03-25 18:41:24 +0700

    Delete 01 directory

--- 01/src/routes/testing-routes.ts
+++
@@ -1,8 +0,0 @@
-import {Request, Response, Router} from "express";
-export const testingRoutes = Router();
-import {allVideos} from "../index";
-
-testingRoutes.delete('/', (req: Request, res: Response) => {
-    allVideos.length = 0;
-    res.send(204)
-})
