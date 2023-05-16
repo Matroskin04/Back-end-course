5f857eb7b767ccfc0ce578552b312607061e8129
Matroskin04 <zetwyn@mail.ru>
2023-05-16 16:52:23 +0700

    transport private keys in secrete file

--- 2_sprint/src/setting.ts
+++ 2_sprint/src/setting.ts
@@ -7,9 +7,6 @@ import {testingRoutes} from "./routes/testing-routes";
 import {commentsRoutes} from "./routes/comments-routes";
 import cookieParser from "cookie-parser";
 
-
-export const PRIVATE_KEY_ACCESS_TOKEN = process.env.PRIVATE_KEY || 'testingAccessKey123' // todo здесь оставить?
-export const PRIVATE_KEY_REFRESH_TOKEN = process.env.PRIVATE_KEY || 'testingRefreshKey123'
 const app = express()
 
 app.use(cookieParser())
