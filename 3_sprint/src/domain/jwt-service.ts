47e0b6cd4f8ae6c68a133d26899ac924fdf0a0db
Matroskin04 <zetwyn@mail.ru>
2023-05-21 00:59:10 +0700

    Changed time expiration in tokens

--- 3_sprint/src/domain/jwt-service.ts
+++ 3_sprint/src/domain/jwt-service.ts
@@ -10,13 +10,13 @@ export const jwtService = {
 
     createAccessToken(userId: string): string  {
 
-        return jwt.sign({userId: userId}, PRIVATE_KEY_ACCESS_TOKEN, {expiresIn: '60s'})
+        return jwt.sign({userId: userId}, PRIVATE_KEY_ACCESS_TOKEN, {expiresIn: '10s'})
     },
 
     createRefreshToken(userId: string, existingDeviceId: string | null): string  {
 
         const deviceId = existingDeviceId ?? randomUUID();
-        return jwt.sign({userId, deviceId}, PRIVATE_KEY_REFRESH_TOKEN, {expiresIn: '120s'})
+        return jwt.sign({userId, deviceId}, PRIVATE_KEY_REFRESH_TOKEN, {expiresIn: '20s'})
     },
 
     async changeTokensByRefreshToken(userId: ObjectId, cookieRefreshToken: string): Promise<AccessRefreshTokens | false> {
