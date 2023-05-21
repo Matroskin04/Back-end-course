9c0d02d426c5683e2ca2739d91476c5c74b4316e
Matroskin04 <zetwyn@mail.ru>
2023-05-21 21:12:02 +0700

    remove tokens from .gitignore (for vercel)

---
+++ 3_sprint/src/tokens.ts
@@ -0,0 +1,2 @@
+export const PRIVATE_KEY_ACCESS_TOKEN = process.env.PRIVATE_KEY || 'testingAccessKey123' // todo куда?
+export const PRIVATE_KEY_REFRESH_TOKEN = process.env.PRIVATE_KEY || 'testingRefreshKey123'
