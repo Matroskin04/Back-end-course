cca18dd2dfbed64984dfcc118066c6324439dc8c
Matroskin04 <zetwyn@mail.ru>
2023-05-21 11:58:40 +0700

    vercel changed import

--- 3_sprint/src/setting.ts
+++ 3_sprint/src/setting.ts
@@ -8,7 +8,7 @@ import {commentsRoutes} from "./routes/comments-routes";
 import cookieParser from "cookie-parser";
 import {devicesRoutes} from "./routes/devices-routes";
 
-const app = express()
+export const app = express()
 
 app.set('trust proxy', true)
 
@@ -25,5 +25,3 @@ app.use('/hometask-03/auth', authRoutes);
 app.use('/hometask-03/comments', commentsRoutes);
 app.use('/hometask-03/devices', devicesRoutes);
 app.use('/hometask-03/testing/all-data', testingRoutes);
-
-export default app
