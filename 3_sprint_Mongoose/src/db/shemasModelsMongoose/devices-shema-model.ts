9ccbe10782066e83adaabbf64bacb023f5a6ca37
Matroskin04 <zetwyn@mail.ru>
2023-07-03 14:44:03 +0900

    Trying to do auto deletion

--- 3_sprint_Mongoose/src/db/shemasModelsMongoose/devices-shema-model.ts
+++ 3_sprint_Mongoose/src/db/shemasModelsMongoose/devices-shema-model.ts
@@ -5,11 +5,11 @@ import {DeviceType} from "../../repositories/repositories-types/devices-types-re
 export const DeviceSchema = new mongoose.Schema<WithId<DeviceType>>({
     ip: {type: String, required: true},
     title: {type: String, required: true},
-    lastActiveDate: {type: String, required: true},
+    lastActiveDate: {type: String, required: true, expires: '10s'},
     deviceId: {type: String, required: true},
     userId: {type: String, required: true},
-    expirationDate: {type: Number, required: true}
+    expirationDate: {type: Number, required: true},
 }, { autoIndex: false });
 export const DeviceModel = mongoose.model<WithId<DeviceType>>('devices', DeviceSchema);
 //indexes:
-DeviceSchema.index( { lastActiveDate: 1 }, { expires: '10s' } ); // todo чистка девайсов через индексы? Сделать
+// DeviceSchema.index( { la: 1 }, { expires: '20s' } ); // todo чистка девайсов через индексы? Сделать
