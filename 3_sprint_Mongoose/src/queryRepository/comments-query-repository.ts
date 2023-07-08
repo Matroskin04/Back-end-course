c7c64dc29301ee1edbac4fc98e31d4763b49427c
Matroskin04 <zetwyn@mail.ru>
2023-07-08 10:14:13 +0900

    Like status. Fixed logic in updating status like comment

--- 3_sprint_Mongoose/src/queryRepository/comments-query-repository.ts
+++ 3_sprint_Mongoose/src/queryRepository/comments-query-repository.ts
@@ -20,17 +20,17 @@ export class CommentsQueryRepository  {
             return null;
         }
 
-        // let myStatus: 'Like' | 'Dislike' | 'None' //todo типизация
-        // const likeInfo = await this.likesInfoQueryRepository.getLikesInfoByCommentAndUser(new ObjectId(commentId), new ObjectId(comment.commentatorInfo.userId));
-        //
-        // if (!likeInfo) {
-        //     myStatus = 'None';
-        //
-        // } else {
-        //     myStatus = likeInfo.statusLike;
-        // }
+        let myStatus: 'Like' | 'Dislike' | 'None' //todo типизация
+        const likeInfo = await this.likesInfoQueryRepository.getLikesInfoByCommentAndUser(new ObjectId(commentId), new ObjectId(comment.commentatorInfo.userId));
 
-        return mappingComment(comment, 'None'); //todo статус кого? У нас нет юзера.
+        if (!likeInfo) {
+            myStatus = 'None';
+
+        } else {
+            myStatus = likeInfo.statusLike;
+        }
+
+        return mappingComment(comment, myStatus); //todo статус кого? У нас нет юзера.
     }
 
     async getCommentsOfPost(query: QueryPostModel, id: string): Promise<CommentOfPostPaginationType | null> {
