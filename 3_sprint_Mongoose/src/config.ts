e8daaed9d4129de285a383c8de9da56a41676519
Matroskin04 <zetwyn@mail.ru>
2023-07-24 17:10:03 +0900

    For vercel, add file

---
+++ 3_sprint_Mongoose/src/config.ts
@@ -0,0 +1,4 @@
+export const env = {
+    PRIVATE_KEY_ACCESS_TOKEN: process.env.PRIVATE_KEY || 'testingAccessKey123',
+    PRIVATE_KEY_REFRESH_TOKEN: process.env.PRIVATE_KEY || 'testingRefreshKey123'
+}
