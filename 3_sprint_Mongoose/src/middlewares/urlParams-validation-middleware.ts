a3bfac309a6cdddae246a0982a78cac62a4a7058
Matroskin04 <zetwyn@mail.ru>
2023-07-24 14:25:39 +0900

    fixed function validation url format

--- 3_sprint_Mongoose/src/middlewares/urlParams-validation-middleware.ts
+++ 3_sprint_Mongoose/src/middlewares/urlParams-validation-middleware.ts
@@ -3,13 +3,12 @@ import {ObjectId} from "mongodb";
 
 export const validateFormatOfUrlParams = async (req: Request, res: Response, next: NextFunction) => {
 
-    if (req.params.blogId) {
-        req.params.id = req.params.blogId;
-    }
 
-    if ( !ObjectId.isValid(req.params.id) ) {
-        res.status(404).send('Incorrect value of id (wrong format)');
-        return;
+    for (let i in req.params) {
+        if ( !ObjectId.isValid(req.params[i]) ) {
+            res.status(404).send('Incorrect value of id (wrong format)');
+            return;
+        }
     }
     next();
 }
