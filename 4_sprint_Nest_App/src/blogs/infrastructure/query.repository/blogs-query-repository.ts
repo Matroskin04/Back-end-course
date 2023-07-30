11bfeec92af8db99bb07f59306107cc90bd1f3f0
Matroskin04 <zetwyn@mail.ru>
2023-07-30 20:20:50 +0900

    did sort default be _id (for tests)

--- 4_sprint_Nest_App/src/blogs/infrastructure/query.repository/blogs-query-repository.ts
+++ 4_sprint_Nest_App/src/blogs/infrastructure/query.repository/blogs-query-repository.ts
@@ -19,7 +19,6 @@ export class BlogsQueryRepository {
   async getAllBlogs(query: QueryBlogModel): Promise<BlogPaginationType> {
     const searchNameTerm: string | null = query?.searchNameTerm ?? null;
     const paramsOfElems = await variablesForReturn(query);
-    if (paramsOfElems.sortBy === 'createdAt') paramsOfElems.sortBy = '_id';
 
     const countAllBlogsSort = await this.BlogModel.countDocuments({
       name: { $regex: searchNameTerm ?? '', $options: 'i' },
