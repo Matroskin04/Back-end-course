1e8bd75caba6cd3722e7c9a2edf4483108426050
Matroskin04 <zetwyn@mail.ru>
2023-08-09 12:48:57 +0900

    15 homework 1) fixed blog id guard (changed error to exception, userId to user.id)

--- 4_sprint_Nest_App/src/infrastructure/guards/blog-owner-by-id.guard.ts
+++ 4_sprint_Nest_App/src/infrastructure/guards/blog-owner-by-id.guard.ts
@@ -1,4 +1,9 @@
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  NotFoundException,
+} from '@nestjs/common';
 import { NotFoundError } from 'rxjs';
 import { BlogsSAQueryRepository } from '../../features/blogs/super-admin-blogs/infrastructure/query.repository/blogs-sa.query.repository';
 import { ObjectId } from 'mongodb';
@@ -9,13 +14,13 @@ export class BlogOwnerByIdGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
 
-    if (!request.userId) throw new Error('userId is not found');
+    if (!request.user?.id) throw new Error('userId is not found');
 
     const blog = await this.blogsSAQueryRepository.getBlogById(
       new ObjectId(request.params.blogId),
     );
-    if (!blog) throw new NotFoundError('This blog is not found');
+    if (!blog) throw new NotFoundException('This blog is not found');
 
-    return blog.blogOwnerInfo.userId === request.userId;
+    return blog.blogOwnerInfo.userId === request.user.id;
   }
 }
